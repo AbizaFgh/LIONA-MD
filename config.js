1591095a38a891350f06eed69c99245a835e51a3
AbizaFgh <162008953+AbizaFgh@users.noreply.github.com>
2025-12-23 13:27:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  token: '8423986609:AAH39tFMHeO_yAi-3RaqEfSWZGkuJV1p5V4',
-  ownerId: '5632941237',
+  owner: ["5632941237"],
+  botToken: process.env.BOT_TOKEN || "8423986609:AAH39tFMHeO_yAi-3RaqEfSWZGkuJV1p5V4",
+  prefix: ".",
   botMode: 'self',
   apiProvider: 'https://zelapioffciall.koyeb.app',  
   botName: 'LIONA-MD'
