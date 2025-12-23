1553cb4faff235201bf77a5a846eb5b5ee44f1a5
AbizaFgh <162008953+AbizaFgh@users.noreply.github.com>
2025-12-23 13:30:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  owner: ["5632941237"],
-  botToken: process.env.BOT_TOKEN || "8423986609:AAH39tFMHeO_yAi-3RaqEfSWZGkuJV1p5V4",
+  ownerID: ["5632941237"],
+  token : "8423986609:AAH39tFMHeO_yAi-3RaqEfSWZGkuJV1p5V4",
   prefix: ".",
   botMode: 'self',
   apiProvider: 'https://zelapioffciall.koyeb.app',  
